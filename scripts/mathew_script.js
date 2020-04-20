4e3574ab6190eeed9648929328ceb3a92c9c6932
ageorge11 <ageorg11@confederationcollege.ca>
2020-04-20 18:44:45 -0400

    commitng the script for top, bottom, left and rigth button

--- scripts/mathew_script.js
+++ scripts/mathew_script.js
@@ -1,12 +1,10 @@
 //Image List - from the local floder
-var imageList = ["./eldhose/1.jpg", "./eldhose/2.jpg", "./eldhose/3.jpg", "./eldhose/4.jpg", "./eldhose/5.jpg",
-"./eldhose/6.jpg", "./eldhose/7.jpg", "./eldhose/8.jpg", "./eldhose/9.jpg", "./eldhose/10.jpg",
-"./eldhose/11.jpg", "./eldhose/12.jpg", "./eldhose/13.jpg", "./eldhose/14.jpg", "./eldhose/15.jpg",
-"./eldhose/16.jpg", "./eldhose/17.jpg", "./eldhose/18.jpg", "./eldhose/19.jpg", "./eldhose/20.jpg",
-"./eldhose/21.jpg", "./eldhose/22.jpg", "./eldhose/23.jpg", "./eldhose/24.jpg", "./eldhose/25.jpg",
-"./eldhose/26.jpg", "./eldhose/27.jpg", "./eldhose/28.jpg", "./eldhose/29.jpg", "./eldhose/30.jpg",];
-//Image List - Online links
-//var ImageList = ["https://i.picsum.photos/id/1004/5616/3744.jpg", "https://i.picsum.photos/id/1003/1181/1772.jpg", "https://i.picsum.photos/id/1001/5616/3744.jpg", "https://i.picsum.photos/id/1000/5626/3635.jpg"]
+var imageList = ["./mathew/1.jpg", "./mathew/2.jpg", "./mathew/3.jpg", "./mathew/4.jpg", "./mathew/5.jpg",
+"./mathew/6.jpg", "./mathew/7.jpg", "./mathew/8.jpg", "./mathew/9.jpg", "./mathew/10.jpg",
+"./mathew/11.jpg", "./mathew/12.jpg", "./mathew/13.jpg", "./mathew/14.jpg", "./mathew/15.jpg",
+"./mathew/16.jpg", "./mathew/17.jpg", "./mathew/18.jpg", "./mathew/19.jpg", "./mathew/20.jpg",
+"./mathew/21.jpg", "./mathew/22.jpg", "./mathew/23.jpg", "./mathew/24.jpg", "./mathew/25.jpg",
+"./mathew/26.jpg", "./mathew/27.jpg", "./mathew/28.jpg", "./mathew/29.jpg", "./mathew/30.jpg",];
 
 // Run when the page is loaded
 window.onload = function () {
@@ -43,6 +41,7 @@ function clearImages() {
     }
 }
 
+//resizes the images to large medium and small
 function resize(element) {
     var img = element.parentNode.previousSibling;
     var imgSize = element.innerHTML;
@@ -59,4 +58,60 @@ function resize(element) {
         element.innerHTML = " Medium "
         img.style.width = "40%";
     }
+}
+
+//moves the image to the begining
+function toTop(element) {
+    var img = element.parentNode.previousSibling.getAttribute("src");
+    var imgIndex = imageList.indexOf(img);
+
+    imageList.splice(imgIndex, 1);
+    imageList.unshift(img);
+
+    clearImages();
+    addImage(imageList);
+}
+
+//moves the image to last
+function toBottom(element) {
+    var img = element.parentNode.previousSibling.getAttribute("src");
+    var imgIndex = imageList.indexOf(img);
+
+    imageList.splice(imgIndex, 1);
+    imageList.push(img);
+
+    clearImages();
+    addImage(imageList);
+}
+
+//moves the image 1 position up
+function toLeft(element) {
+    var img = element.parentNode.previousSibling.getAttribute("src");
+    var imgIndex = imageList.indexOf(img);
+
+    if (imgIndex > 0) {
+        swap(imageList, imgIndex, imgIndex - 1)
+    }
+
+    clearImages();
+    addImage(imageList);
+}
+
+//moves the image 1 position down
+function toRight(element) {
+    var img = element.parentNode.previousSibling.getAttribute("src");
+    var imgIndex = imageList.indexOf(img);
+
+    if (imgIndex < imageList.length - 1) {
+        swap(imageList, imgIndex, imgIndex + 1)
+    }
+
+    clearImages();
+    addImage(imageList);
+}
+
+function swap(array, fromIndex, toIndex) {
+    var temp = array[fromIndex];
+    array[fromIndex] = imageList[toIndex];
+    array[toIndex] = temp;
 }
